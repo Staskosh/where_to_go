7ac5175b570ef00b52d4a84d5397cfdb80708e81
staskosh <staskoshenkov@rambler.ru>
2022-04-04 16:25:52 +0300

    Setup media files serving

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+from environs import Env
+
+env = Env()
+env.read_env()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -61,7 +65,8 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': env.str(
+            'DATABASE_FILEPATH', os.path.join(BASE_DIR, 'db.sqlite3')),
     }
 }
 
@@ -121,6 +126,10 @@ TEMPLATES = [
         },
     },
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.template.context_processors import static
+from django.conf.urls.static import static
 from django.urls import path
 
 from places import views
@@ -8,3 +10,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_who_is_online, name='show_who_is_online'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
