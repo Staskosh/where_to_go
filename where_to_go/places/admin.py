10cd59135c58fc43c376d4a0d56ba9ed94d102cb
staskosh <staskoshenkov@rambler.ru>
2022-04-04 14:00:49 +0300

    Add Place,Coordinates models

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from places.models import Place, Coordinates
+
+admin.site.register(Place)
+admin.site.register(Coordinates)
 # Register your models here.
--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
+class Place(models.Model):
+    title = models.CharField('Заголовок', max_length=200)
+    description_short = models.TextField('Краткое описание')
+    description_long = models.TextField('Полное описание')
+
+    def __str__(self):
+        return self.title
+
+class Coordinates(models.Model):
+    title = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        verbose_name='Координаты',
+    )
+    lng = models.FloatField('Долгота')
+    lat = models.FloatField('Широта')
+
+    def __str__(self):
+        return f'{self.title.title}'
+
+
 # Create your models here.
