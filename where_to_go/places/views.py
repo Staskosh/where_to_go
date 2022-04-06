54c26a977049d64a7d15b2a06c1add257da04814
staskosh <staskoshenkov@rambler.ru>
2022-04-06 11:09:36 +0300

    Load places from db.

--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -6,66 +6,32 @@ from django.template.response import TemplateResponse
 from places.models import Place
 
 
-places_geo = {
+def show_places(request):
+    places = Place.objects.all()
+    places_geo = {
         "type": "FeatureCollection",
         "features": '',
     }
-
-
-def show_places(request):
-    # places = Place.objects.all()
-    # places_feature = []
-    # for place in places:
-    #     title = place.title
-    #     print(title)
-    #     placeid = place.placeid
-    #     coordinates = place.coordinates.first()
-    #     # images = place.image.all()
-    #     # image_urls = [image for image in images]
-    #     places_feature.append({
-    #         "type": "Feature",
-    #         "geometry": {
-    #             "type": "Point",
-    #             "coordinates": [coordinates.lat, coordinates.lng]
-    #         },
-    #         "properties": {
-    #             "title": title,
-    #             "placeId": placeid,
-    #             "detailsUrl": f'./static/places/{placeid}.json'
-    #         }
-    #     })
-    # places_geo['features'] = places_feature
-    # print(places_geo)
-
-    places_geo = {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.62, 55.793676]
-          },
-          "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": "./static/places/moscow_legends.json"
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "./static/places/roofs24.json"
-          }
-        }
-      ]
-    }
+    places_feature = []
+    for place in places:
+        title = place.title
+        print(title)
+        placeid = place.placeid
+        coordinates = place.coordinates.first()
+        places_feature.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [coordinates.lng, coordinates.lat]
+            },
+            "properties": {
+                "title": title,
+                "placeId": placeid,
+                "detailsUrl": f'./static/places/{placeid}.json'
+            }
+        })
+    places_geo['features'] = places_feature
+    print(places_geo)
 
     context = {
         'places_json': places_geo
