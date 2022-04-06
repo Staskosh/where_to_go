0d1059559b75a6e7dc187bf1e6274f8003c94bf6
staskosh <staskoshenkov@rambler.ru>
2022-04-06 16:36:28 +0300

    Preview images in admin

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from places.models import Place, Coordinates, Image
 
 
 class ImageInline(admin.TabularInline):
+    fields = ['img', 'get_preview', 'number']
+    readonly_fields = ('get_preview',)
     model = Image
 
+    def get_preview(self, instance):
+        return format_html('<img src="{}" height=200 />'.format(instance.img.url))
 
 
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [
         ImageInline,
     ]
+
 
 admin.site.register(Place, PlaceAdmin)
 admin.site.register(Coordinates)
