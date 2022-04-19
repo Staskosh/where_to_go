587c7028b4d9534b93022cde157e3e262daf11bd
staskosh <staskoshenkov@rambler.ru>
2022-04-19 09:58:01 +0300

    Add the main field for creating new places.

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -1,39 +1,38 @@
 from io import BytesIO
-from os.path import split, splitext
+from os.path import split
 from urllib.parse import urlsplit, unquote
 
 import requests
 from django.core.files import File
 from django.core.management.base import BaseCommand
 from places.models import Image, Place
-from transliterate import translit
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             "place_url",
-            nargs='+',
             help="Please enter the url of the file"
         )
 
     def handle(self, *args, **options):
-        response = requests.get(options['place_url'][0])
+        response = requests.get(options['place_url'])
         response.raise_for_status()
         raw_place = response.json()
 
-        place = Place.objects.get_or_create(
+        place,_ = Place.objects.get_or_create(
             title=raw_place['title'],
-            description_short=raw_place['description_short'],
-            description_long=raw_place['description_long'],
-            placeid=translit(raw_place['title'], reversed=True),
-            lng=float(raw_place['coordinates']['lng']),
-            lat=float(raw_place['coordinates']['lat'])
+            defaults={
+            'description_short': raw_place['description_short'],
+            'description_long': raw_place['description_long'],
+            'lng': float(raw_place['coordinates']['lng']),
+            'lat': float(raw_place['coordinates']['lat'])
+            }
         )
-
-        all_images = place[0].image.count()
         image_links = raw_place['imgs']
         for image_link in image_links:
+            all_images = place.image.count()
+            print(all_images)
             _, raw_image_name = split(urlsplit(image_link).path)
             image_name = unquote(raw_image_name)
             response = requests.get(image_link)
@@ -42,6 +41,6 @@ class Command(BaseCommand):
             buf.write(response.content)
             Image.objects.get_or_create(
                 number=all_images+1,
-                title=place[0],
+                title=place,
                 img=File(buf, image_name)
             )
