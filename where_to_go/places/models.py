622d03d1f2c63d001e8c6a57e807794feea5e47c
staskosh <staskoshenkov@rambler.ru>
2022-05-04 14:56:43 +0300

    Make the coordinates mandatory

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -6,8 +6,8 @@ class Place(models.Model):
     title = models.CharField('Заголовок', max_length=200)
     description_short = models.TextField('Краткое описание', blank=True)
     description_long = HTMLField('Полное описание', blank=True)
-    lng = models.FloatField('Долгота', blank=True, null=True)
-    lat = models.FloatField('Широта', blank=True, null=True)
+    lng = models.FloatField('Долгота')
+    lat = models.FloatField('Широта')
 
     def __str__(self):
         return self.title
